62c2a5bebd127423c00522b8a6aa1b19bc854734
Yash <yashyadav2773@gmail.com>
2026-07-19 16:28:46 +0530

    added mul_nums func and updated mainn.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from utils.calculator import add_nums,div_nums , sub_nums, pow_nums
+from utils.calculator import add_nums,div_nums , sub_nums, pow_nums, mul_nums
 
 if __name__ == "__main__":
     print(add_nums(7,3))
     print(div_nums(4,2))
     print(sub_nums(4,2))
     print(pow_nums(4,2))
+    print(mul_nums(4,2))
+
--- utils/calculator.py
+++ utils/calculator.py
@@ -9,3 +9,6 @@ def sub_nums(a,b):
 
 def pow_nums(a,b):
     return a^b
+
+def mul_nums(a,b):
+    return a*b
