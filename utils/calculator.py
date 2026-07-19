9304fa25f019cef7c075b7a13dd61d411be29bc8
Yash <yashyadav2773@gmail.com>
2026-07-19 18:10:20 +0530

    formatted calculator.py

--- utils/calculator.py
+++ utils/calculator.py
@@ -1,16 +1,16 @@
 def add_nums(a,b):
-    return a+b
+    return a + b
 
 def div_nums(a,b):
-    return a/b
+    return a / b
 
 def sub_nums(a,b):
-    return a-b
+    return a - b
 
 def pow_nums(a,b):
-    return a^b
+    return a ** b
 
 def mul_nums(a,b):
-    return a*b
+    return a * b
 
 ##########
