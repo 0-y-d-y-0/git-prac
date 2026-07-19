7c16d5bf3506924befd987327ba5abca2119574c
Yash <yashyadav2773@gmail.com>
2026-07-19 15:59:46 +0530

    added sub_nums func and updated main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils.calculator import add_nums
+from utils.calculator import add_nums, sub_nums
 
 if __name__ == "__main__":
     print(add_nums(7,3))
+    print(sub_nums(4,2))
 
--- utils/calculator.py
+++ utils/calculator.py
@@ -1,2 +1,5 @@
 def add_nums(a,b):
     return a+b
+
+def sub_nums(a,b):
+    return a-b
